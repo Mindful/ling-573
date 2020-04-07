dad6605ae19868470e26c55f7489512fef288dd6
Joshua Tanner <mindful.jt@gmail.com>
2020-04-07 15:12:03 -0700

    add data and data loading code

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from data import *
+# have to at least import Topic class from data for unpickling to work
+
+def main():
+    #train_topics = get_dataset_topics(TRAIN)
+    dev_topics = get_dataset_topics(DEV_TEST)
+    for topic in dev_topics:
+        print(topic)  # TODO - actual code goes in this loop
+
+
+
+
+if __name__ == '__main__':
+    main()
