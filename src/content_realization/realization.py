83fbbd8d5eb93a38c06ec23d2cb2d9d4ad9ac012
stringguardian15 <warose91@gmail.com>
2020-04-11 13:18:55 -0500

    Unpack selection_object. Change summary to list of strings. Avoid duplicate sentences.

--- src/content_realization/realization.py
+++ src/content_realization/realization.py
@@ -4,37 +4,41 @@ class Realization:
     word_quota = 100
 
     def __init__(self, selection_object):
-        self.selection = selection_object
+        self.selected_content = selection_object.selected_content
+        self.doc_group = selection_object.doc_group
         self.summary = self.summarize()
 
     def summarize(self, complete_sentences = True):
         '''
-        ** Takes words from selection_object until 100 words reached. **
-        ** Consider writing only full sentences to summary, instead of exactly 100 words **
+        ** Takes sentences from selection_object until 100 words reached (only full sentences). **
         ** No thought currently to order between articles **
         :param complete_sentences: True means summary will end with a complete sentence. False will fill to exactly 100 words.
-        :return: summary as a string, 1 sentence per line.
+        :return: summary as a list of strings.
         '''
         total_words = 0
-        summary = ""
+        summary = []
         # Iterate through all spans in selection until we reach 100 words
         quota_reached = False # Marks whether we've reached word quota. Know when to exit outer loop
-        for candidates in self.selection.selected_content.values():
+        for candidates in self.selected_content.values():
             if quota_reached:
                 break
-            for cand in list(candidates):
-                cand = cand.text
+            for cand in [x.text for x in list(candidates)]:
+                #cand = cand.text
+                if cand in summary:
+                    #avoid adding sentences that are exact duplicates
+                    continue
                 remaining_words = self.word_quota - total_words
                 cand_len = len(cand.split()) #I think this is how word count will be measured in evaluation
                 if cand_len <= remaining_words:
                     # if cand will not overfill quota, add whole span to summary
-                    summary = summary+cand+"\n"
+                    summary.append(cand)
                     total_words += cand_len
                 else:
                     if not complete_sentences:
                         # if cand will overfill quota, take only as many words as necessary to reach quota
-                        summary = summary + ' '.join(cand.split()[0:remaining_words])
+                        summary.append(' '.join(cand.split()[0:remaining_words]))
                         total_words += self.word_quota
                     quota_reached = True
                     break
         return summary
+
