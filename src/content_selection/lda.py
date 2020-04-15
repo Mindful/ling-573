d75c873b4aaa9862b0b21f59e70497b33ba7a620
Jacob Hoffman <jrhoff@uw.edu>
2020-04-14 20:27:30 -0400

    LDA working

--- src/content_selection/lda.py
+++ src/content_selection/lda.py
@@ -11,14 +11,25 @@ class LDA:
         self.num_words = 10
         self.run()
 
+    def accept_token(self,token):
+        tok_str = str(token).lower()
+        if token.is_punct or tok_str in STOP_WORDS or tok_str == '`':
+            return False
+        return True
+
     def get_vocab(self,documents):
         # remove punctiation and lowercase everything
-        unique_words = [  str(token).lower() for article in documents.articles
+        unique_words = set([  str(token).lower() for article in documents.articles
                                 for paragraph in article.paragraphs
                                     for sentence in paragraph.sents
                                         for token in sentence
-                                            if not token.is_punct and not token in STOP_WORDS]
-        return { unique_words[i]:i for i in range(len(unique_words)) }
+                                            if self.accept_token(token)])
+        directory = {}
+        i = 0
+        for word in unique_words:
+            directory[word] = i
+            i+=1
+        return directory
 
     def count(self,token_array):
         counts = {word:0 for word in token_array}
@@ -28,9 +39,9 @@ class LDA:
 
     def doc2vec(self,document):
         words = [str(token).lower() for paragraph in document.paragraphs
-                                for sentence in paragraph.sents
-                                    for token in sentence
-                                        if not token.is_punct and not token in STOP_WORDS]
+                                        for sentence in paragraph.sents
+                                            for token in sentence
+                                                if self.accept_token(token)]
         word_counts = self.count(words)
         vector = np.zeros(len(self.vocab))
         for word in word_counts:
@@ -38,9 +49,17 @@ class LDA:
         return vector
 
 
+    def topN_topics(self,model, feature_names, no_top_words):
+            for topic_idx, topic in enumerate(model.components_):
+                print("Topic %d:" % (topic_idx))
+                print(" ".join([feature_names[i]
+                          for i in topic.argsort()[:-no_top_words - 1:-1]]))
+
 
     def run(self):
-        documents = np.stack([self.doc2vec(article) for article in self.docs.articles])
+        document_matrix = np.stack([self.doc2vec(article) for article in self.docs.articles])
         lda = skLDA(n_components=self.num_topics,n_jobs=-1)
-        lda.fit(self.count())
-        print(lda)
+        lda.fit(document_matrix)
+        self.topN_topics(lda,{v:k for k,v in self.vocab.items()},10)
+
+
--- src/content_selection/selection.py
+++ src/content_selection/selection.py
@@ -7,7 +7,7 @@ class Selection:
     def __init__(self,document_group_object):
         self.doc_group = document_group_object
         self.selected_content = { article.id:self.select(article) for article in document_group_object.articles}
-
+        #self.lda()
 
 
     def select(self,document_group_article):
@@ -16,7 +16,7 @@ class Selection:
         :param document_group_article: An object of class DocGroupArticle
         :return : The first sentence of the first and last paragraphs of the article
         """
-        #self.lda()
+
         num_paragraphs = len(document_group_article.paragraphs)
         return ( list(document_group_article.paragraphs[0].sents)[0],
                  list(document_group_article.paragraphs[num_paragraphs-1].sents)[0]
