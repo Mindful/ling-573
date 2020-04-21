23896da5c7c5472319e33737a78dd31ca8f559a2
Joshua Tanner <mindful.jt@gmail.com>
2020-04-20 18:07:17 -0700

    data regeneration local run option

--- src/regenerate_data.py
+++ src/regenerate_data.py
@@ -1,19 +1,34 @@
 import os
 import glob
-from data import DATA_DIR, get_dataset_topics, DATASETS
+import sys
+from data import DATA_DIR, get_dataset_topics, DataManager
+from data.corpora import Aquaint, Aquaint2
 
 '''
-Cleans out the data/ directory and regenerates the results there. Requires no arguments, but must be run on patas.
+Cleans out the data/ directory and regenerates the results there. Must either be passed a local directory including 
+all the corpora directories and topic config files, or run on patas with no arguments.
 '''
+
+
+def configure_local(directory):
+    for name, location in DataManager.datasets.items():
+        DataManager.datasets[name] = os.path.join(directory, os.path.basename(location))
+
+    DataManager.corpora = [Aquaint(directory), Aquaint2(directory)]
 
 
 def main():
+    if len(sys.argv) > 1:
+        local_dir = sys.argv[1]
+        print('Running on local directory', local_dir, '...')
+        configure_local(local_dir)
+
     print('Deleting existing data...')
     files = glob.glob(DATA_DIR+'/*')
     for f in files:
         os.remove(f)
 
-    for dataset in DATASETS:
+    for dataset in DataManager.datasets:
         print('Regenerating data for dataset', dataset)
         get_dataset_topics(dataset)
 
