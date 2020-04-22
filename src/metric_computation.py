2bd5179028bfd8fe6916cc768bcb1d2a5a618f95
Joshua Tanner <mindful.jt@gmail.com>
2020-04-22 14:27:53 -0700

    update metric_computation

--- src/metric_computation.py
+++ src/metric_computation.py
@@ -5,18 +5,23 @@ import os
 import pickle
 from progress.bar import Bar
 from math import log2
+from sklearn.feature_extraction import DictVectorizer
+import scipy.sparse
+import csv
 
 
-WORD_FREQ_BY_DOC = 'word_freq_by_doc.pickle'
+WORD_FREQ_BY_DOC = 'word_freq_by_doc'
 
 def metric_file_name(metric_name, corpus):
     return os.path.join(DATA_DIR, corpus.name + '_' + metric_name)
 
 def compute_word_counts_by_doc(corpus):
 
+    print('Loading spaCy...')
     tokenizer = spacy.load("en_core_web_lg", disable=['tagger', 'parser', 'ner'])
 
-    articles = load_all_articles(corpus)
+    #articles = load_all_articles(corpus)
+    articles = load_sample_articles(corpus, count=2)
     textblob_by_id = {
         article.id: ' '.join(article.paragraphs) for article in articles
     }
@@ -29,7 +34,7 @@ def compute_word_counts_by_doc(corpus):
     bar = Bar('Processing articles...', max=len(textblob_by_id))
     for doc, article_id in tokenizer.pipe(spacy_gen, as_tuples=True):
         for token in doc:
-            if not token.is_punct and not token.is_stop:
+            if not (token.is_punct or token.is_stop or token.like_num or token.like_url or token.like_email):
                 word_counts[article_id][token.lower_] +=1
         bar.next()
 
@@ -37,11 +42,36 @@ def compute_word_counts_by_doc(corpus):
         pickle.dump(word_counts, picklefile)
 
     bar.finish()
+    print('Vectorizing...')
+    vectorizer = DictVectorizer()
+    article_names = list(word_counts.keys())
+    sparse_vector = vectorizer.fit_transform(word_counts.values())
+    vocabulary = vectorizer.get_feature_names()
+
+
+    print('Saving data for sparse vector with shape', sparse_vector.shape, '...')
+    scipy.sparse.save_npz(metric_file_name(WORD_FREQ_BY_DOC+'.vector', corpus), sparse_vector)
+    with open(metric_file_name(WORD_FREQ_BY_DOC+'.articles', corpus), 'w', newline='') as f:
+        wr = csv.writer(f)
+        wr.writerow(article_names)
+
+    with open(metric_file_name(WORD_FREQ_BY_DOC+'.vocab', corpus), 'w', newline='') as f:
+        wr = csv.writer(f)
+        wr.writerow(vocabulary)
+
+    print("Done")
 
 
 def get_words_by_doc(corpus):
-    with open(metric_file_name(WORD_FREQ_BY_DOC, corpus), 'rb') as picklefile:
-        return pickle.load(picklefile)
+    sparse_vector = scipy.sparse.load_npz(metric_file_name(WORD_FREQ_BY_DOC+'.vector.npz', corpus))
+    with open(metric_file_name(WORD_FREQ_BY_DOC+'.vocab', corpus), newline='') as f:
+        vocabulary = next(csv.reader(f))
+
+    with open(metric_file_name(WORD_FREQ_BY_DOC+'.articles', corpus), newline='') as f:
+        articles = next(csv.reader(f))
+
+    return sparse_vector, articles, vocabulary
+
 
 
 def compute_idf(corpus):
