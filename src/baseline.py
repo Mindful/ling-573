69545b8513f4385604fed5d00f1dd69f263eec0b
stringguardian15 <warose91@gmail.com>
2020-05-06 22:14:33 -0500

    created sentence_similarity.py, added bert sim, added config

--- src/baseline.py
+++ src/baseline.py
@@ -15,7 +15,7 @@ def main():
     #Selection.selection_method = Selection.select_simple
 
     bar = Bar('Processing topics...', max=len(topics))
-    for topic in topics:
+    for topic in topics[:2]:
         doc_group = DocumentGroup(topic)
         selected_content = Selection(doc_group)
         realized_content = Realization(selected_content)
