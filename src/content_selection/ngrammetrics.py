881e3cc368ce325f348da82c1a93b7bb79ffb7fd
Jacob Hoffman <jrhoff@uw.edu>
2020-05-13 00:20:44 -0400

    fixing stupid mistake

--- src/content_selection/ngrammetrics.py
+++ src/content_selection/ngrammetrics.py
@@ -6,7 +6,7 @@ from common import Globals
 
 
 class NgramMetrics:
-    def __init__(self,document_group,config,idf_data):
+    def __init__(self,document_group,config):
         self.documents = document_group
         self.config = config
         self.idf = Globals.idf.copy()
